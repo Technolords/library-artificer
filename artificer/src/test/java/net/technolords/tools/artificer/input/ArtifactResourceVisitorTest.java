5fcf44f509715a7b5cb821f178cde8328f789868
Sridevi <jsridevi@gmail.com>
2016-01-05 23:38:34 +0100

    Review Comments updated in code.

--- artificer/src/test/java/net/technolords/tools/artificer/input/ArtifactResourceVisitorTest.java
+++ artificer/src/test/java/net/technolords/tools/artificer/input/ArtifactResourceVisitorTest.java
@@ -10,6 +10,7 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,21 +21,26 @@ import java.nio.file.Path;
 public class ArtifactResourceVisitorTest extends TestSupport {
     private static final Logger LOGGER = LoggerFactory.getLogger(ArtifactResourceVisitorTest.class);
 
+    private static final String INPUT_CLASS     = "class" + File.separator + "ArtificerImplTest.class";
+    private static final String INPUT_JAR       = "jars" + File.separator + "artificer-1.0.0-SNAPSHOT.jar";
+    private static final String INPUT_UNDEFINED = "abc";
+
     /**
      * Test case #1: Test classification of a resource.
      */
+
     @DataProvider(name = "dataSetClassifyResource")
     public Object[][] dataSetClassifyResource() {
         return new Object[][] {
-                {"ArtificerImplTest.class","D:/commit2Learn/techno-tools/artificer/target/test-classes/net/technolords/tools/artificer/"},
-                {"abc","D:/commit2Learn/techno-tools/artificer/src/test/resources/data/"}
+                {INPUT_CLASS},
+                {INPUT_UNDEFINED}
         };
     }
 
     @Test(dataProvider = "dataSetClassifyResource")
-    public void testClassificationOfResource(String input, String location) {
+    public void testClassificationOfResource(String input) {
 
-        Path pathToResourceLocation = FileSystems.getDefault().getPath(location);
+        Path pathToResourceLocation = FileSystems.getDefault().getPath(getPathToDataFolder() + File.separator + input);
         LOGGER.debug("The path towards the input file '" + input + "' exists: " + Files.exists(pathToResourceLocation));
 
         Analysis analysis = new Analysis();
@@ -54,19 +60,20 @@ public class ArtifactResourceVisitorTest extends TestSupport {
     /**
      * Test case #2: Test adding a resource to a resource group.
      */
+
     @DataProvider(name = "dataSetResourceGroupClassification")
     public Object[][] dataSetResourceGroupClassification() {
         return new Object[][] {
-                {"ArtificerImplTest.class","D:/commit2Learn/techno-tools/artificer/target/test-classes/net/technolords/tools/artificer/",".class"},
-                {"abc","D:/commit2Learn/techno-tools/artificer/src/test/resources/data/","_classification_undefined_"},
-                {"artificer-1.0.0-SNAPSHOT.jar","D:/commit2Learn/techno-tools/artificer/src/test/resources/data/jars",".jar"}
+                {INPUT_CLASS, ".class"},
+                {INPUT_UNDEFINED, "_classification_undefined_"},
+                {INPUT_JAR, ".jar"}
         };
     }
 
     @Test(dataProvider = "dataSetResourceGroupClassification")
-    public void testAddResourceToClassificationGroup(String input, String location, String classification) {
+    public void testAddResourceToClassificationGroup(String input, String classification) {
 
-        Path pathToResourceLocation = FileSystems.getDefault().getPath(location);
+        Path pathToResourceLocation = FileSystems.getDefault().getPath(getPathToDataFolder() + File.separator + input);
         LOGGER.debug("The path towards the input file '" + input + "' exists: " + Files.exists(pathToResourceLocation));
 
         Analysis analysis = new Analysis();
