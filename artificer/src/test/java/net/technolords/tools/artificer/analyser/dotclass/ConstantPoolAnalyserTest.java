d7d5f979d946a749bfe426dd5295e73321ef307b
Sridevi <jsridevi@gmail.com>
2016-02-18 16:36:23 +0100

    ConstantPoolAnalyserTest : - added DataProvider - invalidity of Class do not thrown exception at expected method i.e., analyseByteCode

--- artificer/src/test/java/net/technolords/tools/artificer/analyser/dotclass/ConstantPoolAnalyserTest.java
+++ artificer/src/test/java/net/technolords/tools/artificer/analyser/dotclass/ConstantPoolAnalyserTest.java
@@ -5,6 +5,7 @@ import net.technolords.tools.artificer.domain.resource.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -21,11 +22,15 @@ public class ConstantPoolAnalyserTest extends TestSupport {
     private static final Logger LOGGER = LoggerFactory.getLogger(BytecodeParserTest.class);
     private static final String KNOWN_JAVA_SPECIFICATIONS_REFERENCE_FILE = "analyser/dotclass/java-specifications.xml";
 
-    @Test
-    public void testReferencedClassesExtraction() {
-
-        final String CLASSNAME = "ErrorProcessor.class"; //for Empty class "EOFException" is thrown
-        final Set<String> expectedClasses = new HashSet<String>(Arrays.asList("java/lang/Object","net/technolords/tools/artificer/exception/ArtificerException","net/technolords/tools/artificer/Analyser"));
+    @DataProvider(name = "dataSetClasses")
+    public Object[][] dataSet() {
+        return new Object[][] {
+                { "Analyser.class" , Arrays.asList("java/lang/Object","net/technolords/tools/artificer/exception/ArtificerException","net/technolords/tools/artificer/Analyser")},
+                { "abc.class", null }
+        };
+    }
+    @Test(dataProvider = "dataSetClasses")
+    public void testReferencedClassesExtraction(final String CLASSNAME, final List<String> expectedClasses) {
 
         Path pathToResourceLocation = FileSystems.getDefault().getPath(super.getPathToClassFolder().toAbsolutePath() + File.separator + CLASSNAME);
         LOGGER.debug("The path towards the class file '" + CLASSNAME + "' exists: " + Files.exists(pathToResourceLocation));
@@ -34,14 +39,27 @@ public class ConstantPoolAnalyserTest extends TestSupport {
         Resource resource = new Resource();
         resource.setPath(pathToResourceLocation);
         resource.setName(CLASSNAME);
-        resource.setCompiledVersion("1.8");
 
-        BytecodeParser bytecodeParser = new BytecodeParser(KNOWN_JAVA_SPECIFICATIONS_REFERENCE_FILE);
-        bytecodeParser.analyseBytecode(resource);
+        // resource.isValidClass(); // first try without using try catch
 
-        ConstantPoolAnalyser constantPoolAnalyser = new ConstantPoolAnalyser();
-        Set<String> referencedClasses = constantPoolAnalyser.extractReferencedClasses(resource.getConstantPool());
+        // below try catch is second try
+        // however the NullPointer Exception is thrown at the failure of Assertion, and not when analyseByteCode method is called.
+       try{
 
-        Assert.assertEquals(referencedClasses, expectedClasses);
+           if (resource.isValidClass()) {
+               resource.setCompiledVersion("1.8");
+
+               BytecodeParser bytecodeParser = new BytecodeParser(KNOWN_JAVA_SPECIFICATIONS_REFERENCE_FILE);
+               bytecodeParser.analyseBytecode(resource);
+
+               ConstantPoolAnalyser constantPoolAnalyser = new ConstantPoolAnalyser();
+               Set<String> referencedClasses = constantPoolAnalyser.extractReferencedClasses(resource.getConstantPool());
+
+               Assert.assertEquals(referencedClasses, expectedClasses);
+           }
+        }catch (Exception e){
+           e.printStackTrace();
+       }
+
     }
 }
