d46e209eb4549a210bf07226fe04ac0119cd787b
Technolords <mike@technolords.net>
2016-09-12 22:13:26 +0200

    Removal of obsolete code and update of release plugin

--- src/test/java/net/technolords/tools/artificer/TestSupport.java
+++ src/test/java/net/technolords/tools/artificer/TestSupport.java
@@ -38,19 +38,11 @@ public class TestSupport {
 
         // Set path to folder containing the data, i.e. src/test/resources/data
         Path pathToData = FileSystems.getDefault().getPath(buffer.toString());
-//        if(!pathToData.toAbsolutePath().toString().contains("artificer")) {
-//            StringBuilder pathWithPrefix = new StringBuilder();
-//            pathWithPrefix.append("artificer").append(File.separator).append(buffer.toString());
-//            pathToData = FileSystems.getDefault().getPath(pathWithPrefix.toString());
-//        }
         this.pathToDataFolder = pathToData.toAbsolutePath();
         LOGGER.debug("Data folder set: {} and exists: {}", this.pathToDataFolder.toString(), Files.exists(this.pathToDataFolder));
 
         // Set path to Target folder to create Output, i.e. target
         Path pathToTarget = FileSystems.getDefault().getPath("target");
-//        if(!pathToTarget.toAbsolutePath().endsWith("artificer" + File.separator + "target")) {
-//            pathToTarget = FileSystems.getDefault().getPath("artificer" + File.separator + "target");
-//        }
         this.pathToTargetFolder = pathToTarget.toAbsolutePath();
         LOGGER.debug("Target folder set: {} and exists: {}", this.pathToTargetFolder.toString(), Files.exists(this.pathToTargetFolder));
 
